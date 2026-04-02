adc1ef8cce33d7917da29c189a608448e540da91
Russell Oje <russelloje@gmail.com>
2026-04-02 15:01:35 +0100

    Update default theme

--- portfolio.config.ts
+++ portfolio.config.ts
@@ -428,7 +428,7 @@ const CONFIG = {
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: { id: '', snippetVersion: 6 },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'caramellatte',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
